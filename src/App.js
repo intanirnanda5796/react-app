f9ae84306b3d9f1ae715531a1edb4d67c5968915
intan irnanda <45584774+intanirnanda5796@users.noreply.github.com>
2021-05-28 17:17:55 +0700

    add git ignore and embedding expression in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'intan irnanda'
+const element = <h1>Hello, {name}</h1>
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{element}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
