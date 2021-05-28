6d2d5b5ace1e730bc1657063501163ce7d62dba2
intan irnanda <45584774+intanirnanda5796@users.noreply.github.com>
2021-05-28 17:24:30 +0700

    handling event

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-const name = 'intan irnanda'
-const element = <h1>Hello, {name}</h1>
+const name = "intan irnanda";
+const element = <h1>Hello, {name}</h1>;
 
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{element}</p>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Submit</button>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
